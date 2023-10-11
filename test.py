d2940a80e809478c51e0ae876447d92233788b15
Dev-98 <devparker48@gmail.com>
2023-10-11 18:33:40 +0530

    Bot Code

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
+from langchain.schema import HumanMessage
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from dotenv import load_dotenv, find_dotenv
 import os
 
-print(os.getenv("OPENAI_API_KEY"))
+text = input("enter : ")
+
+# print(messages[content])
+try:
+    # Load environment variables from .env file
+    load_dotenv(find_dotenv())
+
+    # Access the OpenAI API key
+    api_key = os.getenv("OPENAI_API_KEY")
+    print("api : ",api_key)
+
+    llm = OpenAI(openai_api_key = api_key)
+
+    feetful_of_fun_name = llm.predict_messages(text)[0]['content']
+    
+    print(feetful_of_fun_name)
+
+except Exception as e:
+    print(str(e))
